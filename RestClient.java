1251a8cad1f9a906df38d77bbb17205595a50003
veeresh0555 <veeru.veeresh0555@gmail.com>
2019-11-08 11:13:00 +0530

    RestClient.java
    RestClient client

---
+++ RestClient.java
@@ -0,0 +1,11 @@
+public class RestClient{
+
+public static void main(String args[]){
+OrderController ocontroller=new OrderController;
+//Call individual methods like below
+
+ocontroller.getCustomers();
+
+}
+
+}
