04de982311cdbc6af3975880fd82c46e7739abb1
veeresh0555 <veeru.veeresh0555@gmail.com>
2019-11-08 10:35:55 +0530

    Order.java
    order java file

---
+++ Order.java
@@ -0,0 +1,27 @@
+public class Order{
+
+private int orderid;
+private int cid;
+private String oitem;
+
+public int getCid() {
+        return Cid;
+    }
+    public void setCid(int Cid) {
+        this.Cid = Cid;
+    }
+    public int getOrderid() {
+        return Orderid;
+    }
+    public void setorderid(int orderid) {
+        this.Orderid = orderid;
+    }
+    public String getOitem() {
+        return oitem;
+    }
+    public void setIsbn(String Oitem) {
+        this.Oitem = oitem;
+    }
+    
+
+}
