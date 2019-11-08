354483ca8fa1e5370b1101d5a55af4be25a91027
veeresh0555 <veeru.veeresh0555@gmail.com>
2019-11-08 10:29:11 +0530

    Customer.java
    new file for customer creation

---
+++ Customer.java
@@ -0,0 +1,26 @@
+
+public class Customer{
+
+private int cid;
+private String cName;
+
+public int getCid() {
+        return Cid;
+    }
+    public void setIsbn(int Cid) {
+        this.Cid = Cid;
+    }
+
+public String getCName() {
+        return CName;
+    }
+    public void setCName(String CName) {
+        this.CName = CName;
+    }
+
+
+}
+
+
+
+}
